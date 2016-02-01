b406cda8d9d56bd47a053367ddf86661f2290fdc
damien5314 <damien5314@gmail.com>
2016-02-01 13:59:18 -0800

    Showing spinner while loading application data

--- app/src/main/java/ddiehl/batchuninstaller/MainActivity.kt
+++ app/src/main/java/ddiehl/batchuninstaller/MainActivity.kt
@@ -1,70 +1,98 @@
 package ddiehl.batchuninstaller
 
+import android.app.ProgressDialog
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.find
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.setContentView
+import rx.android.schedulers.AndroidSchedulers
+import rx.lang.kotlin.observable
+import rx.schedulers.Schedulers
+import timber.log.Timber
 import java.util.*
 
 class MainActivity : AppCompatActivity(), MainView {
-  private val _data: MutableList<App> = ArrayList()
+
+  private var mLoadingOverlay: ProgressDialog? = null
+  private var mData: List<App> = emptyList()
+  private var mRecyclerView: RecyclerView? = null
+  private val mAdapter: AppAdapter = AppAdapter(this)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     UI().setContentView(this)
+    mRecyclerView = find<RecyclerView>(R.id.recycler_view)
+    mRecyclerView?.adapter = mAdapter
+    mLoadingOverlay = ProgressDialog(this, R.style.ProgressDialog)
+    mLoadingOverlay?.setCancelable(false)
+    mLoadingOverlay?.setProgressStyle(ProgressDialog.STYLE_SPINNER)
     showSpinner()
     loadData()
-    dismissSpinner()
   }
 
   private fun loadData() {
-    val packageList: List<PackageInfo>
-        = packageManager.getInstalledPackages(PackageManager.GET_ACTIVITIES)
-    val packageSet: MutableSet<String> = HashSet()
-    val pm = packageManager
-    packageList.forEach {
-      val intent = pm.getLaunchIntentForPackage(it.packageName)
-      if (intent != null && !it.packageName.startsWith("com.android")) {
-//        Timber.d(it.packageName + "  -  " + intent.component.className)
-        packageSet.add(it.packageName)
+    observable<List<App>> { subscriber ->
+      val packageList: List<PackageInfo>
+          = packageManager.getInstalledPackages(PackageManager.GET_ACTIVITIES)
+      val packageSet: MutableSet<String> = HashSet()
+      packageList.forEach {
+        val intent = packageManager.getLaunchIntentForPackage(it.packageName)
+        if (intent != null && !it.packageName.startsWith("com.android")) {
+          packageSet.add(it.packageName)
+        }
       }
+      Thread.sleep(1000)
+      subscriber.onNext(
+          packageSet.map {
+            App(
+                packageManager.getApplicationLabel(packageManager.getApplicationInfo(it, 0)),
+                0, // TODO Calculate size of app
+                it
+            )
+          })
+      subscriber.onCompleted()
     }
-    for (packageName in packageSet) {
-      val applicationInfo = packageManager.getApplicationInfo(packageName, 0)
-      _data.add(
-          App(
-              packageManager.getApplicationLabel(applicationInfo),
-              0, // TODO Calculate size of app
-              packageName
-          ))
-    }
+        .subscribeOn(Schedulers.computation())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe({
+          dismissSpinner()
+          mData = it
+          Timber.d("Loaded data (%s)", mData.size)
+          mAdapter.notifyDataSetChanged()
+        },
+            { },
+            { dismissSpinner() })
   }
 
-  override fun getNumItems(): Int = _data.size
+  override fun getNumItems(): Int = mData.size
 
   override fun getItemAt(position: Int): App {
-    return _data[position]
+    return mData[position]
   }
 
   override fun showSpinner() {
-    // TODO
+    mLoadingOverlay?.show();
   }
 
   override fun dismissSpinner() {
-    // TODO
+    if (mLoadingOverlay!!.isShowing) {
+      mLoadingOverlay?.dismiss();
+    }
   }
 }
 
 private class UI : AnkoComponent<MainActivity> {
   override fun createView(ui: AnkoContext<MainActivity>) = ui.apply {
-    val recyclerView = recyclerView() {
+    recyclerView {
+      id = R.id.recycler_view
       layoutManager = LinearLayoutManager(ui.owner)
-      adapter = AppAdapter(ui.owner)
     }
   }.view
 }
