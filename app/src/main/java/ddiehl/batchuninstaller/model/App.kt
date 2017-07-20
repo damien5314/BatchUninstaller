417ffa890ff19301d708232f9ff559a7b6fb4bf1
Damien Diehl <damien5314@gmail.com>
2017-07-19 23:12:14 -0700

    refactoring

--- app/src/main/java/ddiehl/batchuninstaller/model/App.kt
+++ app/src/main/java/ddiehl/batchuninstaller/model/App.kt
@@ -7,13 +7,14 @@ import android.text.TextUtils
 data class App(
         val name: CharSequence,
         var size: Long,
-        val packageName: String) : Parcelable {
+        val packageName: String
+) : Parcelable {
 
     constructor(p0: Parcel) : this(
             TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(p0),
             p0.readLong(),
-            p0.readString()) {
-    }
+            p0.readString()
+    )
 
     override fun writeToParcel(p0: Parcel, p1: Int) {
         TextUtils.writeToParcel(name, p0, 0)
@@ -26,7 +27,16 @@ data class App(
     }
 
     override fun toString(): String {
-        return "" +
-                name + " - " + size
+        return "$name :: $size"
+    }
+
+    companion object CREATOR : Parcelable.Creator<App> {
+        override fun createFromParcel(parcel: Parcel): App {
+            return App(parcel)
+        }
+
+        override fun newArray(size: Int): Array<App?> {
+            return arrayOfNulls(size)
+        }
     }
 }
