92d7dfe258b0d53b3c40c589e24dc9f166d6c187
Damien Diehl <damien5314@gmail.com>
2017-09-18 09:22:19 -0700

    Add Crashlytics

--- app/src/main/java/ddiehl/batchuninstaller/BatchUninstallerApp.kt
+++ app/src/main/java/ddiehl/batchuninstaller/BatchUninstallerApp.kt
@@ -1,17 +1,16 @@
 package ddiehl.batchuninstaller
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class BatchUninstallerApp : Application() {
 
-    companion object {
-        lateinit var app: BatchUninstallerApp
-    }
-
     override fun onCreate() {
         super.onCreate()
-        app = this
+
+        Fabric.with(this, Crashlytics())
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
